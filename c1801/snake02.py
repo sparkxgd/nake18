c9cfc2ad09d35a1503d63b0faed5c8a367acac8b
xiao <472036660@qq.com>
2020-10-16 09:51:13 +0800

    2020年10月16日09:50:01 1班

--- c1801/snake02.py
+++ c1801/snake02.py
@@ -3,9 +3,8 @@
 凯里学院-1801
 贪吃蛇游戏
 功能实现：
-1、利用pygame打开游戏窗体
-2、画出蛇，并移动
-3、键盘控蛇
+1、键盘控蛇
+2、
 '''
 
 import pygame
@@ -17,19 +16,48 @@ window = pygame.display.set_mode((500,500))
 
 x= 0
 y= 0
+# 蛇的方向
+direction = "right"
 #  背景色
 b_color = (153,50,204)
 # 蛇头的颜色
 snake_head_color = (0,255,0)
 while True:
+    # 监听键盘
     for even in pygame.event.get():
         if even.type == pygame.QUIT:
             # 退出
             sys.exit()
-    pygame.time.wait(100)
-    x += 5
-    y += 5
+        elif even.type == pygame.KEYDOWN or even.type==pygame.KEYUP:
+            if even.key == pygame.K_UP:
+                direction = "up"
+            elif even.key == pygame.K_DOWN:
+                direction = "down"
+            elif even.key == pygame.K_LEFT:
+                direction = "left"
+            elif even.key == pygame.K_RIGHT:
+                direction = "right"
+
+    if direction == "right":
+        x += 5
+    elif direction == "left":
+        x -= 5
+    elif direction == "up":
+        y -= 5
+    elif direction == "down":
+        y += 5
+
     window.fill(b_color)
+    # 边缘控制
+    if 0<x and x <500:
+        pass
+    else:
+        text = pygame.font.SysFont("宋体", 48)
+        text_fm = text.render("eating food", 1, (255, 123, 22))
+        window.blit(text_fm, (100, 100))
+
+    pygame.time.wait(100)
+
     # 画蛇
     pygame.draw.rect(window,snake_head_color,(x,y,50,50))
     pygame.display.update()
